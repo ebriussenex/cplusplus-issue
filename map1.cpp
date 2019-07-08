d655c8fad0eb4e4fa5e6fe36e3bd28850a675975
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2019-07-09 01:56:07 +0300

    Create map1.cpp

---
+++ map1.cpp
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#define pass (void)0
+
+#include <limits>
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <fstream>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <list>
+#include <cstring>
+
+std::vector <std::string> PalindromFilter(std::vector <std::string> words, int minLength) {
+	std::vector <std::string> ans;
+	bool flag = true;
+	for (auto word : words) {
+		int n = word.length() - 1;
+		for (int i = 0; i < n / 2 + 1; i++) {
+			if (word[i] == word[n - i]);
+			else {
+				flag = false;
+			}
+		}
+		if (flag && word.length() >= minLength) {
+			ans.push_back(word);
+		}
+		flag = true;
+	}
+	return ans;
+}
+
+bool IsPalindrom(std::string word) {
+	int n = word.length() - 1;
+	for (int i = 0; i < n / 2 + 1; i++) {
+		if (word[i] == word[n - i]);
+		else {
+			return false;
+		}
+	}
+	return true;
+}
+
+void UpdateIfGreater(const int& first, int& second) {
+	if (first > second) {
+		second = first;
+	}
+}
+
+void MoveStrings(std::vector <std::string>& source, std::vector <std::string>& destination) {
+	for (auto s : source) {
+		destination.push_back(s);
+	}
+	source.clear();
+}
+
+//void Reverse(std::vector<int>& v) {
+//	std::reverse(std::begin(v), std::end(v));
+//}
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::string stra, strb;
+	std::map <char, int> mapa;
+	std::map<char, int> mapb;
+	for (int i = 0; i < n; i++) {
+		std::cin >> stra >> strb;
+		for (auto word : stra) {
+			mapa[word]++;
+		}
+		for (auto word : strb) {
+			mapb[word]++;
+		}
+		if (mapa == mapb) std::cout << "YES" << std::endl;
+		else std::cout << "NO" << std::endl;
+		mapa.clear();
+		mapb.clear();
+	}
+	system("PAUSE");
+	return 0;
+}
