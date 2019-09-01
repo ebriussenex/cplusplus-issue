3eed30ba176f23aa3c196741f43e62180d9f3667
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2019-09-01 22:13:34 +0300

    dev

--- yellow_belt/601final/database.cpp
+++ yellow_belt/601final/database.cpp
@@ -4,16 +4,18 @@
 
 void Database::Add(const Date& date, const std::string& event)
 {
-	if (!d_e_.count({ date, event })) {
-		db_for_last_[date].push_back(event);
-		d_e_.insert({ date, event });
+	if (!d_e_[date].count(event)) {
+		db_sort_[date].push_back(event);
+		std::sort(db_sort_[date].begin(), db_sort_[date].end());
+		db_last_[date].push_back(event);
+		d_e_[date].insert(event);
 	}
 }
 
 void Database::Print(std::ostream& os) const
 {
-	for (const auto& item : db_for_last_) {
-		for (const auto& jtem : item.second) {
+	for (auto& item : db_sort_) {
+		for (auto& jtem : item.second) {
 			os << item.first << " " << jtem << std::endl;
 		}
 	}
@@ -21,87 +23,13 @@ void Database::Print(std::ostream& os) const
 
 DbRow Database::Last(const Date& date) const
 {
-	if (db_for_last_.empty()) throw std::invalid_argument("No entries");
-	auto it = db_for_last_.upper_bound(date);
-	if (it != db_for_last_.begin()) {
+	if (db_last_.empty()) throw std::invalid_argument("No entries");
+	auto it = db_last_.upper_bound(date);
+	if (it != db_last_.begin()) {
 		auto itt = std::prev(it);
 		if (!itt->second.empty()) {
 			return{ itt->first, itt->second.back() };
 		}
 	}
 	else throw std::invalid_argument("No entries");
-	/*if (it == db_for_last_.begin() && date != it->first) {
-		throw std::invalid_argument("No entries");
-	}
-	if (it == db_for_last_.end()) it = std::prev(it);*/
-	/*return { it->first, it->second.back() };*/
 }
-
-//template <typename Func>
-//int Database::RemoveIf(Func& predicate) {
-//	int count = 0;
-//	for (const auto& row : db_for_last_) {//deletes events due to condition
-//		Date& date = row.first;
-//		auto it = row.second.stable_partition([predicate, date](std::string event) {
-//			return predicate(date, event);
-//			});
-//		count += it - row.second.begin();
-//		row.second.erase(row.second.begin(), it);
-//		if (row.second.empty()) {
-//			db_for_last_.erase(row.first);
-//		}
-//	}
-//	return count;
-//}
-
-//template<typename Func>
-//std::vector<DbRow> Database::FindIf(Func& predicate) const {
-//	std::vector<DbRow> found;
-//	for (const auto& row : db_for_last_) {
-//		for (const auto& event : row.second) {
-//			if (predicate(row.first, event)) found.push_back({ row.first, event });
-//		}
-//	}
-//	return found;
-//}
-
-//template<typename Func>
-//std::vector<DbRow> Database::FindIf(Func& predicate) const {
-//	std::vector<DbRow> found;
-//	for (const auto& row : db_for_last_) {
-//		for (const auto& event : row.second) {
-//			if (predicate(row.first, event)) found.push_back({ row.first, event });
-//		}
-//	}
-//	return found;
-//}
-//
-//template <typename Func>
-//int Database::RemoveIf(Func predicate) {
-//	unsigned int size_to_del = 0, count = 0;
-//	for (auto& row : db_for_last_) {//deletes events due to condition
-//		Date date = row.first;
-//		auto it = stable_partition(row.second.begin(), row.second.end(),
-//			[predicate, date](std::string event) {
-//				return !predicate(date, event);
-//			});
-//		if (it != row.second.end()) size_to_del = row.second.end() - it;
-//		/*row.second.erase(row.second.begin(), it);
-//		if (row.second.empty()) {
-//			db_for_last_.erase(row.first);
-//		}*/
-//		for (size_t i = 0; i < size_to_del; i++) {
-//			if (!row.second.empty()) {
-//				d_e_.erase({ date, row.second.back() });
-//				row.second.pop_back();
-//				count++;
-//			}
-//			else {
-//				db_for_last_.erase(date);
-//			}
-//		}
-//	}
-//	return count;
-//}
-//
-
--- yellow_belt/601final/database.h
+++ yellow_belt/601final/database.h
@@ -15,6 +15,7 @@ using DbLast = std::map<Date, std::vector<std::string>>;
 
 class Database {
 public:
+	std::map<Date, std::vector<std::string>> to_print_;
 	Database() {};
 	void Add(const Date& date, const std::string& event);
 	void Print(std::ostream& os) const;
@@ -23,13 +24,11 @@ public:
 	int RemoveIf(Func predicate) {
 		unsigned int size_to_del = 0, count = 0;
 		std::vector <Date> clean;
-		for (auto& row : db_for_last_) {//deletes events due to condition
-			Date date = row.first;
-			auto it = stable_partition(row.second.begin(), row.second.end(),
-				[predicate, date](std::string event) {
-					return !predicate(date, event);
-				});
-			size_to_del = row.second.end() - it;
+		for (auto& row : db_sort_) {//deletes events due to condition
+			size_to_del = 0;
+			auto date = row.first;
+			auto it = stable_partition(row.second.begin(), row.second.end(), [predicate, date](std::string event) {return !predicate(date, event); });
+			if (it != row.second.end()) size_to_del = row.second.end() - it;
 			if (it == row.second.begin()) {
 				clean.push_back(date);
 				count += row.second.size();
@@ -37,35 +36,48 @@ public:
 			else {
 				for (size_t i = 0; i < size_to_del; ++i) {
 					if (!row.second.empty()) {
-						d_e_.erase({ date, row.second.back() });
+						d_e_.erase(date);
+						db_last_[date].erase(std::remove(db_last_[date].begin(), db_last_[date].end(), row.second.back()), db_last_[date].end());
+						if (db_last_[date].empty()) db_last_.erase(date);
 						row.second.pop_back();
 						count++;
 					}
-					else {
-						clean.push_back(date);
-					}
+					else clean.push_back(date);
 				}
 			}
 		}
 		for (const auto& date : clean) {
-			db_for_last_.erase(date);
+			d_e_.erase(date);
+			db_sort_.erase(date);
+			db_last_.erase(date);
 		}
+		/*PrintDbLast();*/
 		return count;
 	}
 
 	template<typename Func>
-	std::vector<DbRow> FindIf(Func predicate) const {
-		std::vector<DbRow> found;
-		for (const auto& row : db_for_last_) {
+	std::set<DbRow> FindIf(Func predicate) const {
+		std::set<DbRow> found;
+		for (auto& row : db_sort_) {
 			for (const auto& event : row.second) {
-				if (predicate(row.first, event)) found.push_back({ row.first, event });
+				if (predicate(row.first, event)) found.insert({ row.first, event });
 			}
 		}
 		return found;
 	}
+
+	void PrintDbLast() {
+		for (const auto& item : db_last_) {
+			for (const auto& jtem : item.second) {
+				std::cout << "L: " << item.first << " " << jtem << std::endl;
+			}
+		}
+	}
+
 	DbRow Last(const Date& date) const;
 private:
 	Db db_;
-	std::map<Date, std::vector<std::string>> db_for_last_;
-	std::set <DbRow> d_e_;
+	std::map<Date, std::vector<std::string>> db_sort_;
+	std::map<Date, std::vector<std::string>> db_last_;
+	std::map <Date, std::set<std::string>> d_e_;
 };
