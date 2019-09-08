aefb672fa3b4401339ddf8f6cbfcc42e4e1a8f00
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2019-09-08 22:31:14 +0300

    aaa

---
+++ red_belt/103airline_ticket/airline_ticket.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+struct Date {
+	int year, month, day;
+};
+
+struct Time {
+	int hours, minutes;
+};
+
+struct AirlineTicket {
+	std::string from;
+	std::string to;
+	std::string airline;
+	Date departure_date;
+	Time departure_time;
+	Date arrival_date;
+	Time arrival_time;
+	int price;
+};
+
+std::ostream& operator<<(std::ostream& os, const Date& date);
+std::ostream& operator<<(std::ostream& os, const Time& time);
+
+bool operator<(const Date& lhs, const Date& rhs);
+bool operator==(const Date& lhs, const Date& rhs);
+bool operator<(const Time& lhs, const Time& rhs);
+bool operator==(const Time& lhs, const Time& rhs);
+bool operator!=(const Date& lhs, const Date& rhs);
+bool operator!=(const Time& lhs, const Time& rhs);
