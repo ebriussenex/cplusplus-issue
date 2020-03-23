8f5187705bb91764272632136b3880acf6ce3ed2
Athsoatle <zaharucheba@gmail.com>
2020-03-23 12:48:43 +0300

    split

---
+++ red_belt/5week/03Split_into_words.cpp
@@ -0,0 +1,135 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#define pass (void)0
+
+#include <limits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <random>
+
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cfenv>
+#include <iomanip>
+
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <list>
+#include <forward_list>
+#include <bitset>
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <regex>
+#include <iterator>
+#include <numeric>
+
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <functional>
+
+#include "test_runner.h"
+#include "profile.h"
+#include "simple_vector.h"
+
+
+template <typename Token>
+using Sentence = std::vector<Token>;
+
+
+template <typename Token>
+std::vector<Sentence<Token>> SplitIntoSentences(std::vector<Token> tokens) {
+	std::vector<Sentence<Token>> target;
+	Sentence<Token> sentence;
+	for (size_t i = 0; i < tokens.size(); i++) {
+		if (sentence.empty()) {
+				sentence.push_back(std::move(tokens[i]));
+		}
+		else if (!sentence.empty()) {
+			if (sentence.back().IsEndSentencePunctuation()) {
+				if (!tokens[i].IsEndSentencePunctuation()) {
+					target.push_back(std::move(sentence));
+					sentence.clear();
+				}
+			}
+			sentence.push_back(std::move(tokens[i]));
+		}
+		if (i == tokens.size() - 1) {
+			target.push_back(std::move(sentence));
+		}
+	}
+	return target;
+}
+
+
+struct TestToken {
+	std::string data;
+	bool is_end_sentence_punctuation = false;
+
+	bool IsEndSentencePunctuation() const {
+		return is_end_sentence_punctuation;
+	}
+	bool operator==(const TestToken& other) const {
+		return data == other.data && is_end_sentence_punctuation == other.is_end_sentence_punctuation;
+	}
+};
+
+std::ostream& operator<<(std::ostream& stream, const TestToken& token) {
+	return stream << token.data;
+}
+
+void TestSplitting() {
+	ASSERT_EQUAL(
+		SplitIntoSentences(std::vector<TestToken>({ 
+													{"Split"},
+													{"into"},
+													{"sentences"},
+													{"!"} 
+													})),
+		std::vector<Sentence<TestToken>>({
+			{{"Split"}, {"into"}, {"sentences"}, {"!"}}
+			})
+	);
+
+	ASSERT_EQUAL(
+		SplitIntoSentences(std::vector<TestToken>({ {"Split"}, {"into"}, {"sentences"}, {"!", true} })),
+		std::vector<Sentence<TestToken>>({
+			{{"Split"}, {"into"}, {"sentences"}, {"!", true}}
+			})
+	);
+
+	ASSERT_EQUAL(
+		SplitIntoSentences(std::vector<TestToken>({ {"Split"}, {"into"}, {"sentences"}, {"!", true}, {"!", true}, {"Without"}, {"copies"}, {".", true} })),
+		std::vector<Sentence<TestToken>>({
+			{{"Split"}, {"into"}, {"sentences"}, {"!", true}, {"!", true}},
+			{{"Without"}, {"copies"}, {".", true}},
+			})
+			);
+}
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	TestRunner tr;
+	RUN_TEST(tr, TestSplitting);
+	return 0;
+}
---
+++ red_belt/5week/03simple_vector/Source.cpp
@@ -0,0 +1,75 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#define pass (void)0
+
+#include <limits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <random>
+
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cfenv>
+#include <iomanip>
+
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <list>
+#include <forward_list>
+#include <bitset>
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <regex>
+#include <iterator>
+#include <numeric>
+
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <functional>
+
+#include "test_runner.h"
+#include "profile.h"
+#include "simple_vector.h"
+
+
+void TestCopyAssignment() {
+	SimpleVector<int> numbers(10);
+	std::iota(numbers.begin(), numbers.end(), 1);
+
+	SimpleVector<int> dest;
+	ASSERT_EQUAL(dest.Size(), 0u);
+
+	dest = numbers;
+	ASSERT_EQUAL(dest.Size(), numbers.Size());
+	ASSERT(dest.Capacity() >= dest.Size());
+	ASSERT(std::equal(dest.begin(), dest.end(), numbers.begin()));
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	TestRunner tr;
+	RUN_TEST(tr, TestCopyAssignment);
+	return 0;
+}
